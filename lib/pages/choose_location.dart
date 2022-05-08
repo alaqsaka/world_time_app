03053b7614c81a2ca8701a74fb6b57da5e11868d
alaqsaka <aqsaabidin15@gmail.com>
2022-05-09 00:00:06 +0700

    Asynchronous Code

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -9,12 +9,27 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
+  // asynchronus
+  void getData () async {
+    // simulate network request for a username
+    String agent = await Future.delayed(Duration(seconds: 3), () {
+      return 'yoru';
+    });
+
+    String role = await Future.delayed(Duration(seconds: 2), () {
+      return 'duelist';
+    });
+
+    print('$agent - $role');
+  }
+
   int counter = 0;
 
   @override
   void initState() {
     super.initState();
     print('InitState function ran');
+    getData();
   }
 
   @override
