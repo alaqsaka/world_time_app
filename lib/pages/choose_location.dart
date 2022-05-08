5aab53aa06bbf4de28a3e7bbde4bf31b9c44e20c
alaqsaka <aqsaabidin15@gmail.com>
2022-05-09 00:11:24 +0700

    instaling dependency, fetch data

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -9,29 +9,6 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
 
-  // asynchronus
-  void getData () async {
-    // simulate network request for a username
-    String agent = await Future.delayed(Duration(seconds: 3), () {
-      return 'yoru';
-    });
-
-    String role = await Future.delayed(Duration(seconds: 2), () {
-      return 'duelist';
-    });
-
-    print('$agent - $role');
-  }
-
-  int counter = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    print('InitState function ran');
-    getData();
-  }
-
   @override
   Widget build(BuildContext context) {
     print('build function ran');
@@ -43,12 +20,6 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: RaisedButton(
-          onPressed: () {
-            setState(() {
-              counter +=1;
-            });
-      },child: Text('counter $counter')),
     );
   }
 }
