aeeac2d4c336a89b398d472ae07e24ed3c522b35
alaqsaka <aqsaabidin15@gmail.com>
2022-05-09 23:35:52 +0700

    feat: updating time when user click listpile in choose_location.dart:

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -21,6 +21,19 @@ class _ChooseLocationState extends State<ChooseLocation> {
     WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
   ];
 
+  void updateTime(index) async {
+    WorldTime instance = locations[index];
+    await instance.getTime();
+    // navigate to home page
+    Navigator.pop(context, {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDayTime
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     print('build function ran');
@@ -40,7 +53,7 @@ class _ChooseLocationState extends State<ChooseLocation> {
             child: Card(
               child: ListTile(
                 onTap: (){
-                  print(locations[index].location);
+                  updateTime(index);
                 },
                 title: Text(locations[index].location),
                 leading: CircleAvatar(
